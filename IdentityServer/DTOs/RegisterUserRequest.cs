788f3eb28bd35877ad37cc35482d897b4e5adaa7
Inozpavel <inozpavel@mail.ru>
2021-04-05 20:00:45 +0300

    Added account controller

---
+++ IdentityServer/DTOs/RegisterUserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServer.DTOs
+{
+    public class RegisterUserRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
