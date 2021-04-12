6ee95cff1a75656a35885cbffcfd5c1ffebd334d
Inozpavel <inozpavel@mail.ru>
2021-04-12 20:18:29 +0300

    Unused constructors in class ApiException removed

--- Tasks.Api/Exceptions/ApiException.cs
+++ Tasks.Api/Exceptions/ApiException.cs
@@ -5,15 +5,7 @@ namespace Tasks.Api.Exceptions
     [Serializable]
     public abstract class ApiException : Exception
     {
-        protected ApiException()
-        {
-        }
-
         protected ApiException(string message) : base(message)
-        {
-        }
-
-        protected ApiException(string message, Exception inner) : base(message, inner)
         {
         }
 
