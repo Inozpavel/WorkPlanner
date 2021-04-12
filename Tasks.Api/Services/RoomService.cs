e3f69cec2c94e8c646529dd8d45b4d0b53664d57
Inozpavel <inozpavel@mail.ru>
2021-04-12 20:36:37 +0300

    UserService added

--- Tasks.Api/Services/RoomService.cs
+++ Tasks.Api/Services/RoomService.cs
@@ -17,10 +17,13 @@ namespace Tasks.Api.Services
 
         private readonly UnitOfWork _unitOfWork;
 
-        public RoomService(IMapper mapper, UnitOfWork unitOfWork)
+        private readonly UserService _userService;
+
+        public RoomService(IMapper mapper, UnitOfWork unitOfWork, UserService userService)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+            _userService = userService;
         }
 
         public async Task<RoomViewModel> CreateRoom(RoomRequest request, Guid userId)
@@ -47,8 +50,7 @@ namespace Tasks.Api.Services
             if (room == null)
                 throw new NotFoundException("Room with given id was not found!");
 
-            if (!(await CheckUserHasRole(roomId, userId, Roles.Creator) ||
-                  await CheckUserHasRole(roomId, userId, Roles.Administrator)))
+            if (!await _userService.CheckUserHasAnyRole(roomId, userId, Roles.Creator, Roles.Administrator))
                 throw new AccessRightException("The user has insufficient rights");
 
             room = _mapper.Map(request, room);
@@ -65,15 +67,12 @@ namespace Tasks.Api.Services
             if (room == null)
                 throw new NotFoundException("Room with given id was not found!");
 
-            if (!await CheckUserHasRole(roomId, userId, Roles.Creator))
+            if (!await _userService.CheckUserIsInRole(roomId, userId, Roles.Creator))
                 throw new AccessRightException("Insufficient access rights! Only owner can delete room!");
 
             _unitOfWork.RoomRepository.Delete(room);
             await _unitOfWork.SaveChangesAsync();
         }
-
-        public async Task<bool> CheckUserHasRole(Guid roomId, Guid userId, string role) =>
-            await _unitOfWork.RoomRepository.FindUserInRoomWithRole(roomId, userId, role) != null;
 
         public async Task<IEnumerable<RoomViewModel>> FindRoomsForUser(Guid userId)
         {
@@ -89,8 +88,7 @@ namespace Tasks.Api.Services
             var room = await _unitOfWork.RoomRepository.Find(x => x.RoomId == roomId);
             return _mapper.Map<RoomViewModel>(room);
         }
-
-
+        
         public async Task JoinUserToRoom(Guid roomId, Guid userId)
         {
             var room = await _unitOfWork.RoomRepository.FindRoomWithUsers(roomId);
---
+++ Tasks.Api/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Tasks.Api.Data;
+
+namespace Tasks.Api.Services
+{
+    public class UserService
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public UserService(UnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        public async Task<bool> CheckUserHasAnyRole(Guid roomId, Guid userId, params string[] rolesNames)
+        {
+            foreach (string roleName in rolesNames)
+            {
+                if (await CheckUserIsInRole(roomId, userId, roleName))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> CheckUserIsInRole(Guid roomId, Guid userId, string roleName) =>
+            await _unitOfWork.RoomRepository.FindUserInRoomWithRole(roomId, userId, roleName) != null;
+
+        public async Task<bool> CheckUserIsInRoom(Guid roomId, Guid userId)
+        {
+            var room = await _unitOfWork.RoomRepository.FindRoomWithUsers(roomId);
+            return room?.UsersInRoom.Any(x => x.UserId == userId) ?? false;
+        }
+    }
+}
